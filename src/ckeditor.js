869eb12baa165a5853d2a2d814bdadd17341eb0e
OmarOmeiri <omaromeiri@gmail.com>
2021-06-02 16:14:15 -0300

    first commit

---
+++ src/ckeditor.js
@@ -0,0 +1,182 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// The editor creator to use.
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';//
+import Font from '@ckeditor/ckeditor5-font/src/font';//
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';//
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';//
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';//
+import ImageResize from '@ckeditor/ckeditor5-image/src/ImageResize';
+import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage';//
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed';
+
+import sanitizeHtml from 'sanitize-html';
+
+
+const sanitizerOptions = {
+  allowedTags: [
+  "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4",
+  "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div",
+  "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre",
+  "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
+  "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
+  "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
+  "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "style",
+  "html", 'body', 'head', 'button', 'img'
+],
+disallowedTagsMode: 'discard',
+allowedAttributes: {
+  '*': ['class'],
+  a: [ 'href', 'name', 'target' ],
+  // We don't currently allow img itself by default, but this
+  // would make sense if we did. You could add srcset here,
+  // and if you do the URL is checked for safety
+  img: [ 'src' ]
+},
+// Lots of these won't come up by default because we don't allow them
+selfClosing: [ 'img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta' ],
+// URL schemes we permit
+allowedSchemes: [ 'http', 'https', 'ftp', 'mailto', 'tel' ],
+allowedSchemesByTag: {},
+allowedSchemesAppliedToAttributes: [ 'href', 'src', 'cite' ],
+allowProtocolRelative: true,
+enforceHtmlBoundary: true
+}
+
+export default class ClassicEditor extends ClassicEditorBase {}
+
+// Plugins to include in the build.
+ClassicEditor.builtinPlugins = [
+	Essentials,
+	UploadAdapter,
+	Autoformat,
+	Bold,
+	Italic,
+	BlockQuote,
+	CKFinder,
+	CloudServices,
+	EasyImage,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableToolbar,
+	TextTransformation,
+  Alignment,
+  Font,
+  ImageResize,
+  AutoImage,
+  Base64UploadAdapter,
+  HtmlEmbed,
+];
+
+// Editor configuration.
+ClassicEditor.defaultConfig = {
+	toolbar: {
+		items: [
+      'heading',
+			'|',
+      'fontSize',
+      'fontFamily',
+      'fontColor',
+      'fontBackgroundColor',
+			// 'bold',
+			// 'italic',
+      'alignment',
+			'link',
+			'blockQuote',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'outdent',
+			'indent',
+			'insertTable',
+			'|',
+			'uploadImage',
+			'mediaEmbed',
+      '|',
+      'htmlEmbed',
+			'undo',
+			'redo'
+		]
+	},
+	image: {
+		toolbar: [
+			'imageStyle:full',
+      'imageStyle:alignLeft',
+      'imageStyle:alignRight',
+			'|',
+			'imageTextAlternative'
+		],
+    styles: [
+      // This option is equal to a situation where no style is applied.
+      'full',
+
+      // This represents an image aligned to the left.
+      'alignLeft',
+
+      // This represents an image aligned to the right.
+      'alignRight'
+    ]
+	},
+  htmlEmbed:{
+    showPreviews: true,
+    sanitizeHtml(inputHtml){
+      console.log(inputHtml)
+      const clean = sanitizeHtml(inputHtml, sanitizerOptions);
+      console.log(clean)
+      return {
+        html: clean,
+        hasChanged: true,
+      }
+    }
+
+  },
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells'
+		]
+	},
+	// This value must be kept in sync with the language defined in webpack.config.js.
+	language: 'pt-br'
+};
